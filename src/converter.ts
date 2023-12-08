26140c437a38e4ea98d093c467e64b7c73de2174
Alexandra von Criegern <alexandra@evolvedbinary.com>
2023-12-08 17:46:37 +0100

    doc(modules): Add first documentation comments

--- src/converter.ts
+++ src/converter.ts
@@ -8,18 +8,25 @@ import { JDita } from "./classes";
 export async function xditaToJdita(xml: string, abortOnError = true): Promise<DocumentNode> {
   return new Promise((resolve, reject) => {
     const errors: Error[] = [];
+    // Create Parser Object
     const parser = new saxes.SaxesParser({
       xmlns: true,
       fragment: false,
       position: true
     });
 
+    // Create a new document node
     const doc = new DocumentNode();
+
+    // Create an array of Document nodes (type BaseNode)
     const stack: BaseNode[] = [doc];
+
+    // Parse the text and add a new node item to the node-array
     parser.on("text", function (text) {
       stack[stack.length - 1].add(createNode(text), false);
     });
 
+    // Look for the first open tag `<` and add the node to the array
     parser.on("opentag", function (node: saxes.SaxesTagNS) {
       try {
         const obj = createNode(node);
@@ -30,10 +37,12 @@ export async function xditaToJdita(xml: string, abortOnError = true): Promise<Do
       }
     });
 
+    // Look for the close tag `>` and delete the node from the array
     parser.on("closetag", function () {
       stack.pop();
     });
 
+    // return the document tree if run without errors
     parser.on("end", function () {
       if (errors.length && abortOnError) {
         reject(errors);
@@ -41,14 +50,17 @@ export async function xditaToJdita(xml: string, abortOnError = true): Promise<Do
         resolve(doc);
       }
     });
+
     parser.on("error", function (e) {
       errors.push(e);
     });
 
+    // process the xml using the parser
     parser.write(xml).close();
   });
 }
 
+// Convert document tree to json
 export async function xditaToJson(xml: string, abortOnError = true): Promise<JDita> {
   return xditaToJdita(xml, abortOnError).then(doc => doc.json);
 }
