aa985945a1f54fb5aa09fc21517213fffbc2ecb2
Alexandra von Criegern <alexandra@evolvedbinary.com>
2024-01-02 19:28:49 +0100

    [doc] Apply comment headers for video.ts

--- src/nodes/video.ts
+++ src/nodes/video.ts
@@ -7,22 +7,62 @@ import { makeComponent, BaseNode, makeAll, Constructor } from "./base";
 import { SizeNode, SizeFields, isValidSizeField, makeSize } from "./size";
 import { BasicValue } from "../classes";
 
+/**
+ * Define all allowed `video` fields:
+ * 'props', 'dir', 'xml:lang', 'translate', 'id', 'conref', 'outputclass', 'class', 'width', 'height'
+ */
 export const VideoFields = [...FiltersFields, ...LocalizationFields, ...ReuseFields, ...ClassFields, ...SizeFields];
 
+/**
+ * The interface `VideoNode` defines all attribute types for `video`:
+ * 'CDATA', 'NMTOKEN'
+ */
 export interface VideoNode extends FiltersNode, LocalizationNode, ReuseNode, ClassNode, SizeNode { }
 
+/**
+ * Check if the given fields of the `video` node are valid
+ *
+ * @param field - A string containing the name of the field
+ * @param value - A BasicValue-typed value containing the field value
+ * @returns Boolean
+ */
 export const isValidVideoField = (field: string, value: BasicValue): boolean => isValidFiltersField(field, value)
   || isValidLocalizationField(field, value)
   || isValidReuseField(field, value)
   || isValidClassField(field, value)
   || isValidSizeField(field, value);
 
+/**
+ * Check if the `video` node is valid
+ *
+ * @remarks
+ * Assert that the node is an object and has valid attributes
+ *
+ * @param value - The `video` node to test
+ * @returns Boolean
+ */
 export const isVideoNode = (value?: {}): value is VideoNode =>
   typeof value === 'object' && areFieldsValid(VideoFields, value, isValidVideoField);
 
+/**
+ * Construct a `video` node with all available attributes
+ *
+ * @param constructor - The constructor
+ * @returns A `video` node
+ */
 export function makeVideo<T extends Constructor>(constructor: T): T {
   return makeAll(constructor, makeLocalization, makeFilters, makeReuse, makeClass, makeSize);
 }
 
+/**
+ * Create a video node
+ *
+ * @decorator `@makeComponent`
+ * @param XRefNode - The `video` node constructor
+ * @param nodeName - The node name
+ * @param isValidVideoField - A boolean value, if the field is valid or not
+ * @param fields - A List of valid fields
+ * @param childTypes - An Array of allowed child types
+ */
 @makeComponent(makeVideo, 'video', isValidVideoField, VideoFields, ['desc?', 'video-poster?', 'media-controls?', 'media-autoplay?', 'media-loop?', 'media-muted?', 'media-source*', 'media-track*'])
 export class VideoNode extends BaseNode {}
