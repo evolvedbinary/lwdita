a526346f02577ee58bf1ff0bcf2dc98ad4bf2aa8
Alexandra von Criegern <alexandra@evolvedbinary.com>
2024-01-03 11:46:02 +0100

    [doc] Apply comment headers for underlined.ts

--- src/nodes/underlined.ts
+++ src/nodes/underlined.ts
@@ -7,22 +7,65 @@ import { makeComponent, BaseNode, makeAll, Constructor } from "./base";
 import { VariableContentFields, isValidVariableContentField, makeVariableContent } from "./variable-content";
 import { BasicValue } from "../classes";
 
-// TODO:  "+ topic/ph hi-d/u "
+/**
+ * Define all allowed `underlined` fields:
+ * 'dir', 'xml:lang', 'translate', 'keyref', 'class', 'outputclass'
+ *
+ * @privateRemarks
+ *
+ * TODO:  "+ topic/ph hi-d/u "
+ */
 export const UnderlinedFields = [...LocalizationFields, ...VariableContentFields, ...ClassFields];
 
+/**
+ * Interface UnderlinedNode defines the attribute types for `underlined`:
+ * 'CDATA', 'NMTOKEN'
+ */
 export interface UnderlinedNode extends FiltersNode, LocalizationNode, ReuseNode, ClassNode { }
 
+/**
+ * Check if the given fields of the `underlined` node are valid
+ *
+ * @param field - A string containing the name of the field
+ * @param value - A BasicValue-typed value containing the field value
+ * @returns Boolean
+ */
 export const isValidUnderlinedField = (field: string, value: BasicValue): boolean => isValidVariableContentField(field, value)
   || isValidLocalizationField(field, value)
   || isValidClassField(field, value);
 
+/**
+ * Check if the `underlined` node is valid
+ *
+ * @remarks
+ * Assert that the node is an object and has valid attributes
+ *
+ * @param value - The `underlined` node to test
+ * @returns Boolean
+ */
 export const isUnderlinedNode = (value?: {}): value is UnderlinedNode =>
   typeof value === 'object' && areFieldsValid(UnderlinedFields, value, isValidUnderlinedField);
 
+/**
+ * Construct an `underlined` node with all available attributes
+ *
+ * @param constructor - The constructor
+ * @returns An `underlined` node
+ */
 export function makeUnderlined<T extends Constructor>(constructor: T): T {
   return makeAll(constructor, makeLocalization, makeVariableContent, makeClass);
 }
 
+/**
+ * Create an `underlined` node and map the `underlined` node with the HTML tag name `u`
+ *
+ * @decorator `@makeComponent`
+ * @param XRefNode - The `underlined` node constructor
+ * @param nodeName - The Node name
+ * @param isValidUnderlinedField - A boolean value, if the field is valid or not
+ * @param fields - A List of valid fields
+ * @param childTypes - An Array of allowed child types
+ */
 @makeComponent(makeUnderlined, 'u', isValidUnderlinedField, UnderlinedFields, ['%all-inline*'])
 export class UnderlinedNode extends BaseNode {
   static domNodeName = 'u';
