df35c468302a09d600724f0a8c85757cd2a8c802
Alexandra von Criegern <alexandra@evolvedbinary.com>
2024-01-03 16:17:06 +0100

    [doc] Apply comment headers for subscript.ts

--- src/nodes/subscript.ts
+++ src/nodes/subscript.ts
@@ -7,23 +7,66 @@ import { makeComponent, BaseNode, makeAll, Constructor } from "./base";
 import { VariableContentFields, isValidVariableContentField, makeVariableContent } from "./variable-content";
 import { BasicValue } from "../classes";
 
-// TODO:  "+ topic/ph hi-d/sub "
+/**
+ * Define all allowed `subscript` fields:
+ * `dir`, `xml:lang`, `translate`, `keyref`, `class`, `outputclass`
+ *
+ * @privateRemarks
+ * TODO:  "+ topic/ph hi-d/sub "
+ */
 export const SubscriptFields = [...LocalizationFields, ...VariableContentFields, ...ClassFields];
 
+/**
+ * Interface SubscriptNode defines the attribute types for `subscript`:
+ * `CDATA`, `NMTOKEN`
+ */
 export interface SubscriptNode extends FiltersNode, LocalizationNode, ReuseNode, ClassNode { }
 
+/**
+ * Check if the given fields of the `subscript` node are valid
+ *
+ * @param field - A string containing the name of the field
+ * @param value - A BasicValue-typed value containing the field value
+ * @returns Boolean
+ */
 export const isValidSubscriptField = (field: string, value: BasicValue): boolean => isValidVariableContentField(field, value)
   || isValidLocalizationField(field, value)
   || isValidClassField(field, value);
 
+/**
+ * Check if the `subscript` node is valid
+ *
+ * @remarks
+ * Assert that the node is an object and has valid attributes
+ *
+ * @param value - The `subscript` node to test
+ * @returns Boolean
+ */
 export const isSubscriptNode = (value?: {}): value is SubscriptNode =>
   typeof value === 'object' && areFieldsValid(SubscriptFields, value, isValidSubscriptField);
 
+/**
+ * Construct a `subscript` node with all available attributes
+ *
+ * @param constructor - The constructor
+ * @returns A `subscript` node
+ */
 export function makeSubscript<T extends Constructor>(constructor: T): T {
   return makeAll(constructor, makeLocalization, makeVariableContent, makeClass);
 }
 
+/**
+ * Create a `subscript` node and map the `subscript` node with the HTML tag name `sb`
+ *
+ * @decorator `@makeComponent`
+ * @param makeSubscript - The `subscript` node constructor
+ * @param nodeName - A string containing the node name
+ * @param isValidSubscriptField - A boolean value, if the field is valid or not
+ * @param fields - A List of valid fields
+ * @param childNodes - An Array of allowed child nodes: `%all-inline*`
+ */
 @makeComponent(makeSubscript, 'sub', isValidSubscriptField, SubscriptFields, ['%all-inline*'])
 export class SubscriptNode extends BaseNode {
+  /** @override */
   static domNodeName = 'sub';
 }
