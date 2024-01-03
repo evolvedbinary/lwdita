bc590657281f059c0d40797947d476176235282c
Alexandra von Criegern <alexandra@evolvedbinary.com>
2024-01-03 17:31:51 +0100

    [doc] Apply comment headers for ph.ts

--- src/nodes/ph.ts
+++ src/nodes/ph.ts
@@ -6,23 +6,66 @@ import { areFieldsValid } from "../utils";
 import { BaseNode, makeComponent, makeAll, Constructor } from "./base";
 import { BasicValue } from "../classes";
 
+/**
+ * Define all allowed `ph` fields:
+ * `props`, `dir`, `xml:lang`, `translate`, `keyref`, `class`, `outputclass`
+ */
 export const PhFields = [...FiltersFields, ...LocalizationFields, ...VariableContentFields, ...ClassFields];
 
+/**
+ * Interface PhNode defines the attribute type for `ph`: `CDATA`
+ */
 export interface PhNode extends FiltersNode, LocalizationNode, VariableContentNode, ClassNode { }
 
+/**
+ * Check if the given fields of the `ph` node are valid
+ *
+ * @param field - A string containing the name of the field
+ * @param value - A BasicValue-typed value containing the field value
+ * @returns Boolean
+ */
 export const isValidPhField = (field: string, value: BasicValue): boolean => isValidFiltersField(field, value)
   || isValidLocalizationField(field, value)
   || isValidVariableContentField(field, value)
   || isValidClassField(field, value);
 
+/**
+ * Check if the `ph` node is valid
+ *
+ * @remarks
+ * Assert that the node is an object and has valid attributes
+ *
+ * @param value - The `ph` node to test
+ * @returns Boolean
+ */
 export const isPhNode = (value?: {}): value is PhNode =>
   typeof value === 'object' && areFieldsValid(PhFields, value, isValidPhField);
 
+/**
+ * Construct a `ph` node with all available attributes
+ *
+ * @remarks
+ * eslint-disable-next-line `@typescript-eslint/no-explicit-any`
+ *
+ * @param constructor - The constructor
+ * @returns An `ph` node
+ */
 export function makePh<T extends Constructor>(constructor: T): T {
   return makeAll(constructor, makeLocalization, makeFilters, makeVariableContent, makeClass);
 }
 
+/**
+ * Create a `ph` node (span) and map the `ph` node with the HTML tag name `span`
+ *
+ * @decorator `@makeComponent`
+ * @param makePh - The `ph` node constructor
+ * @param nodeName - A string containing the node name
+ * @param isValidPhField - A boolean value, if the field is valid or not
+ * @param fields - A List of valid fields
+ * @param childNodes - An Array of allowed child node `%all-inline*`
+ */
 @makeComponent(makePh, 'ph', isValidPhField, PhFields, ['%all-inline*'])
 export class PhNode extends BaseNode {
+  /** @override */
   static domNodeName = 'span';
 }
