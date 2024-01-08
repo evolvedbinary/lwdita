2294864f56a73ad2fc92136a327ace29399807f1
Alexandra von Criegern <alexandra@evolvedbinary.com>
2024-01-08 12:37:57 +0100

    [doc] Enhance attribute and child node descriptions

--- src/nodes/dt.ts
+++ src/nodes/dt.ts
@@ -7,7 +7,7 @@ import { ReuseFields, ReuseNode, isValidReuseField, makeReuse } from "./reuse";
 import { BasicValue } from "../classes";
 
 /**
- * Define all allowed `dt` fields:
+ * Define all allowed `dt` attributes:
  * `props`, `dir`, `xml:lang`, `translate`, `id`, `conref`, `outputclass`, `class`
  */
 export const DtFields = [...FiltersFields, ...LocalizationFields, ...ReuseFields, ...ClassFields];
@@ -18,11 +18,11 @@ export const DtFields = [...FiltersFields, ...LocalizationFields, ...ReuseFields
 export interface DtNode extends FiltersNode, LocalizationNode, ReuseNode, ClassNode { }
 
 /**
- * Check if the given fields of the `dt` node are valid and matches this list:
+ * Check if the given attributes of the `dt` node are valid and match this list:
  * @See {@link DtFields}
- * 
- * @param field - A string containing the name of the field
- * @param value - A BasicValue-typed value containing the field value
+ *
+ * @param field - A string containing the name of the attribute
+ * @param value - A BasicValue-typed value containing the attribute value
  * @returns Boolean
  */
 export const isValidDtField = (field: string, value: BasicValue): boolean => isValidFiltersField(field, value)
@@ -32,10 +32,10 @@ export const isValidDtField = (field: string, value: BasicValue): boolean => isV
 
 /**
  * Check if the `dt` node is valid
- * 
+ *
  * @remarks
  * Assert that the node is an object and has valid attributes
- * 
+ *
  * @param value - The `dt` node to test
  * @returns Boolean
  */
@@ -43,27 +43,28 @@ export const isDtNode = (value?: {}): value is DtNode =>
   typeof value === 'object' && areFieldsValid(DtFields, value, isValidDtField);
 
 /**
- * Construct an `dt` node with all available attributes
- * 
+ * Construct a `dt` node with all available attributes
+ *
  * @param constructor - The constructor
- * @returns An `dt` node
+ * @returns A `dt` node
  */
 export function makeDt<T extends Constructor>(constructor: T): T {
   return makeAll(constructor, makeLocalization, makeFilters, makeReuse, makeClass);
 }
 
 /**
- * Create an dt node
- * 
+ * Create a `dt` node
+ *
  * @decorator `@makeComponent`
  * @param makeDt - The `dt` node constructor
  * @param nodeName - A string containing the node name
- * @param isValidDtField - A boolean value, if the field is valid or not
- * @param DtFields - An array containing all valid field names See {@link DtFields}
- * @param childNodes - An array containing all valid child node names: `%common-inline*`
- * @returns An `dt` node
+ * @param isValidDtField - A boolean value, if the attribute is valid or not
+ * @param DtFields - An array containing all valid attribute names See {@link DtFields}
+ * @param childNodes - An array containing all valid child node names: `%common-inline*` (`text`, `ph`, `b`, `i`, `u`, `sub`, `sup`, `image`, `data`)
+ * @returns A `dt` node
  */
 @makeComponent(makeDt, 'dt', isValidDtField, DtFields, ['%all-inline*'])
 export class DtNode extends BaseNode {
+  /** @override */
   static domNodeName = 'dt';
 }
