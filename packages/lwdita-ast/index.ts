d1a3cb5b9ab14109c9d6c4c5b0b1e93cb6df14b6
marmoure <tinky93winky@gmail.com>
2024-01-25 15:59:10 +0100

    [feature] split the package export statement

--- packages/lwdita-ast/index.ts
+++ packages/lwdita-ast/index.ts
@@ -16,6 +16,5 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-export * from './classes';
-export * from './utils';
-export * from './converter';
+export * from './factory';
+export * from './nodes';
