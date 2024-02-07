e72addb5a879645855e6cfb4f2e49b4b79b02959
Alexandra von Criegern <alexandra@evolvedbinary.com>
2024-02-07 18:38:24 +0100

    [test] Prepare visitor.ts unit tests

---
+++ packages/lwdita-xml/visitor.spec.ts
@@ -0,0 +1,15 @@
+import { expect, assert } from 'chai';
+import { BasicValue } from "@jdita/lwdita-xml/classes";
+import { XMLTag, Visitor } from './visitor.ts';
+
+// Test class XMLTag:
+// method toString()
+// 1. selfclosing tags
+// 2. start tags
+// 3. end tags
+
+// Test class Visitor:
+// 1. visit()
+// 2. startTag()
+// 3. endTag()
+// 4. selfClosingTag()
