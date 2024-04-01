d635ae4afdc8c4c33a0cd65109b0698097a10a4d
marmoure <tinky93winky@gmail.com>
2024-04-01 10:43:12 +0200

    [bugfix] Remove unsafe type for filters-add

--- packages/lwdita-ast/nodes/filters-adds.ts
+++ packages/lwdita-ast/nodes/filters-adds.ts
@@ -2,6 +2,6 @@
  * FiltersAddsNode are empty
  */
 export const FiltersAddsFields = [];
-export type FiltersAddsNode = {}
+export type FiltersAddsNode = object;
 export const isFiltersAddsNode = (value?: FiltersAddsNode): value is FiltersAddsNode =>
   typeof value === 'object';
