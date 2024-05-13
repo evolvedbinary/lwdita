114f9943c1815a197cfb3c886e983d2f9f8e1154
marmoure <tinky93winky@gmail.com>
2024-05-13 12:51:43 +0200

    [test] WIP alt node attribute test

--- packages/lwdita-ast/test/alt.spec.ts
+++ packages/lwdita-ast/test/alt.spec.ts
@@ -3,3 +3,22 @@ import { AltNode, isAltNode } from "../nodes/alt";
 doNodeTest(AltNode, 'alt', isAltNode,
   ['outputclass', 'class', 'keyref', 'dir', 'xml:lang', 'translate', 'props'],
   ['(text|%ph|%data)*']);
+
+// TODO test the node attributes 
+// something like alt.outputclass reading and writing, making sure the attribute exists on the node and can be 
+// manipulated
+
+
+// eg of wrong case
+// setting alt.attributes does not throw any error
+// if the class does not have the correct attributes it does not throw
+
+describe('AltNode attributes', () => {
+  it("can set correct attributes", () => {
+    //TODO
+  });
+  
+  it("can not set incorrect attributes",() => {
+    //TODO
+  });
+});
