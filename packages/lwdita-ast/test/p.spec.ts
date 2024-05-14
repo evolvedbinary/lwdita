65f62da56fc25c3b285918403cc29a727b7ce893
marmoure <tinky93winky@gmail.com>
2024-05-14 12:17:29 +0200

    [test] test p node attributes

--- packages/lwdita-ast/test/p.spec.ts
+++ packages/lwdita-ast/test/p.spec.ts
@@ -1,5 +1,34 @@
 import { doNodeTest } from "../tests";
 import { PNode, isPNode } from "../nodes/p";
+import { expect } from "chai";
 doNodeTest(PNode, 'p', isPNode,
   ['dir', 'xml:lang', 'translate', 'props', 'id', 'conref', 'outputclass', 'class'],
   ['%all-inline*']);
+
+
+
+describe('P Node attributes', () => {
+  it("can set correct attributes", () => {
+    // create a new node without setting anything. 
+    const p = new PNode({});
+
+    // 'dir', 'xml:lang', 'translate', 'props', 'id', 'conref', 'outputclass', 'class'
+
+    p.dir = "dir";
+    p["xml:lang"] = "lang";
+    p.translate = "translate";
+    p.props = "props";
+    p.id = "id";
+    p.outputclass = "outputclass";
+    p.class = "class";
+
+    expect(p.dir).to.equal("dir");
+    expect(p["xml:lang"]).to.equal("lang");
+    expect(p.translate).to.equal("translate");
+    expect(p.props).to.equal("props");
+    expect(p.id).to.equal("id");
+    expect(p.outputclass).to.equal("outputclass");
+    expect(p.class).to.equal("class");
+
+  });
+});
