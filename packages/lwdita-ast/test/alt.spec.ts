aa7ebb7f96f42a4c01a2e40b426f13e4f6fabe6f
marmoure <tinky93winky@gmail.com>
2024-05-14 11:27:22 +0200

    [test] alt node  attribute test

--- packages/lwdita-ast/test/alt.spec.ts
+++ packages/lwdita-ast/test/alt.spec.ts
@@ -1,24 +1,35 @@
 import { doNodeTest } from "../tests";
 import { AltNode, isAltNode } from "../nodes/alt";
+import { expect } from "chai";
 doNodeTest(AltNode, 'alt', isAltNode,
   ['outputclass', 'class', 'keyref', 'dir', 'xml:lang', 'translate', 'props'],
   ['(text|%ph|%data)*']);
 
-// TODO test the node attributes 
-// something like alt.outputclass reading and writing, making sure the attribute exists on the node and can be 
-// manipulated
 
-
-// eg of wrong case
-// setting alt.attributes does not throw any error
-// if the class does not have the correct attributes it does not throw
 
 describe('AltNode attributes', () => {
   it("can set correct attributes", () => {
-    //TODO
-  });
-  
-  it("can not set incorrect attributes",() => {
-    //TODO
+    // create a new node without setting anything. 
+    const alt = new AltNode({});
+    //go over all the attributes and set them
+    // see if we can get all of the attributes back again
+    // `keyref`, `outputclass`, `class`, `dir`, `xml:lang`, `translate`, `props`
+
+    alt.keyref = "keyref";
+    alt.outputclass = "outputclass";
+    alt.class = "class";
+    alt.dir = "dir";
+    alt["xml:lang"] = "lang";
+    alt.translate = "translate";
+    alt.props = "props";
+
+    expect(alt.keyref).to.equal("keyref");
+    expect(alt.outputclass).to.equal("outputclass");
+    expect(alt.class).to.equal("class");
+    expect(alt.dir).to.equal("dir");
+    expect(alt["xml:lang"]).to.equal("lang");
+    expect(alt.translate).to.equal("translate");
+    expect(alt.props).to.equal("props");
+    
   });
 });
