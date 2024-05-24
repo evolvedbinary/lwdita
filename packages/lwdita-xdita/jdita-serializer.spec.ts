4fa82c045a6ec6b8a348667c0fc097279c306f61
marmoure <tinky93winky@gmail.com>
2024-05-24 15:31:44 +0200

    [tempfix] create a jdita serializer

---
+++ packages/lwdita-xdita/jdita-serializer.spec.ts
@@ -0,0 +1,106 @@
+/*!
+Copyright (C) 2020 Evolved Binary
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+// this is a temporary fix to serialize the JDita tree to XDita by by YB, signed-off by AR.
+import { expect } from 'chai';
+import { JditaSerializer } from './jdita-serializer';
+import { InMemoryTextSimpleOutputStreamCollector } from './stream';
+import { DocumentNode, TextNode, TitleNode, TopicNode } from "@evolvedbinary/lwdita-ast"
+
+describe('jditaSerializer', () => {
+  let outStream: InMemoryTextSimpleOutputStreamCollector;
+  let serializer: JditaSerializer;
+
+  beforeEach(() => {
+    outStream = new InMemoryTextSimpleOutputStreamCollector();
+    serializer = new JditaSerializer(outStream);
+  });
+
+  it('skip the document node', () => {
+    // test setup
+    const document = new DocumentNode();
+    const topic = new TopicNode();
+    document.add(topic);
+    const title = new TitleNode();
+    topic.add(title);
+
+    const jdita = document.json
+
+    // perform serialization
+    serializer.serializeFromJdita(jdita);
+
+    // expect the output stream to contain the correct XML
+    expect(outStream.getText()).equal("<topic><title/></topic>");
+  });
+
+  it('serialize a document with indentation', () => {
+    // test setup
+    const document = new DocumentNode();
+    const topic = new TopicNode();
+    document.add(topic);
+    const title = new TitleNode();
+    topic.add(title);
+
+    // configure serializer with indentation
+    serializer = new JditaSerializer(outStream, true, ' ', 4);
+
+    const jdita = document.json
+
+    // perform serialization
+    serializer.serializeFromJdita(jdita);
+
+    // expect the output stream to contain the correct XML with indentation
+    expect(outStream.getText()).equal("<topic>\n    <title/>\n</topic>\n");
+  });
+
+  it('serialize a document with text content', () => {
+    // test setup
+    const document = new DocumentNode();
+    const topic = new TopicNode();
+    document.add(topic);
+    const title = new TitleNode();
+    topic.add(title);
+    const text = new TextNode('Hello World');
+    title.add(text);
+
+    const jdita = document.json
+
+    // perform serialization
+    serializer.serializeFromJdita(jdita);
+
+    // expect the output stream to contain the correct XML with text content
+    expect(outStream.getText()).equal("<topic><title>Hello World</title></topic>");
+  });
+
+  it('serialize a document with attributes', () => {
+    // test setup
+    const document = new DocumentNode();
+    const topic = new TopicNode();
+    document.add(topic);
+    const title = new TitleNode({ dir: 'ltr', class: 'title' });
+    topic.add(title);
+
+    const jdita = document.json
+
+    // perform serialization
+    serializer.serializeFromJdita(jdita);
+
+    // expect the output stream to contain the correct XML with attributes
+    expect(outStream.getText()).equal('<topic><title dir="ltr" class="title"/></topic>');
+  });
+
+});
---
+++ packages/lwdita-xdita/jdita-serializer.ts
@@ -0,0 +1,148 @@
+/*!
+Copyright (C) 2020 Evolved Binary
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+// this is a temporary fix to serialize the JDita tree to XDita by by YB, signed-off by AR.
+import { TextSimpleOutputStream } from "./stream";
+import { JDita } from "./classes";
+
+/**
+ * Serializer for XDITA.
+ * Takes an Jdita and serializes it to XDITA.
+ */
+export class JditaSerializer {
+  outputStream: TextSimpleOutputStream;
+  indent: boolean;
+  tabSize: number;
+  indentation: string;
+  EOL: string;
+  depth = 0;
+
+  /**
+   * Constructor
+   *
+   * @param outputStream - The output stream.
+   * @param indent - enable indentation
+   * @param indentation - the character (or string) to use as the indent
+   * @param tabSize - size of the tab, only used when the `indentation` is not a `\t` character.
+   */
+  constructor(outputStream: TextSimpleOutputStream, indent = false, indentation = " ", tabSize = 4) {
+    this.outputStream = outputStream;
+    this.indent = indent;
+    this.tabSize = tabSize;
+    this.indentation = indentation;
+    if(indentation === '\t') {
+      this.tabSize = 1;
+    }
+    this.EOL = '\n';
+  }
+
+  /**
+   * Serialize the indentation to the output stream
+   */
+  private serializeIndentation(): void {
+    if (this.indent) {
+      this.outputStream.emit(this.indentation.repeat(this.depth * this.tabSize));
+    }
+  }
+
+  /**
+   * Serialize the End of Line character to the output stream
+   */
+  private serializeEOL(): void {
+    if (this.indent) {
+      this.outputStream.emit(this.EOL);
+    }
+  }
+
+  /**
+   * Serialize a jdita element to the output stream.
+   *
+   * @param jdita - the element to serialize
+   */
+  private serializeJDitaElement(jdita: JDita): void {
+    // serialize the start of the element start tag
+    this.outputStream.emit(`<${jdita.nodeName}`);
+    // serialize the attributes
+    this.serializeAttributes(jdita);
+    if (jdita.children?.length) {
+      // as the element has children or attributes, serialize the remainder of the element start tag
+      this.outputStream.emit(`>`);
+      this.serializeEOL();
+      // increment the depth after starting an element
+      this.depth++;
+      // visit the element's children
+      jdita.children.forEach(child => this.serializeFromJdita(child));
+      // decrement the depth after serializing the elements children
+      this.depth--;
+      this.serializeIndentation();
+      this.outputStream.emit(`</${jdita.nodeName}>`);
+    } else {
+      // element has no attributes or children, so the remainder of the element start tag as a self-closing element
+      this.outputStream.emit(`/>`);
+    }
+  }
+
+  /**
+   * Serialize the attributes to the output stream
+   *
+   * @param jdita - the node to serialize the attributes of
+   */
+  private serializeAttributes(jdita: JDita): void {
+    let attrsStr = '';
+    const props = jdita.attributes;
+    if (props) {
+      const attr = props as Record<string, string>;
+      attrsStr = Object.keys(props).filter(key => attr[key]).map(key => `${key}="${attr[key]}"`).join(' ');
+    }
+    if (attrsStr.length) {
+      attrsStr = ` ${attrsStr}`;
+    }
+    this.outputStream.emit(attrsStr);
+  }
+
+  /**
+   * Travel Jdita tree and serialize it to the output stream
+   *
+   * @param jdita - the Jdita tree to serialize
+   */
+  serializeFromJdita(jdita: JDita): void {
+    if (jdita.nodeName === 'document') {
+
+      jdita.children?.forEach(child => this.serializeFromJdita(child));
+
+      // close the output stream as we have now serialized the document
+      this.outputStream.close();
+
+    } else {
+      // serialize any indentation
+      this.serializeIndentation();
+
+      if (jdita.nodeName === 'text') {
+        // if the node is a text node, serialize its text content
+        if (jdita.content) {
+          this.outputStream.emit(String(jdita.content));
+        }
+
+      } else {
+        this.serializeJDitaElement(jdita);
+      }
+
+      // serialize any EOL
+      this.serializeEOL();
+    }
+  }
+}
