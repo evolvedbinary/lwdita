39839c16e100b78d5ef6566597b8aa275326d60d
Alexandra von Criegern <alexandra@evolvedbinary.com>
2024-07-11 17:51:50 +0200

    [feature] Update node `video` & extend unit test

--- packages/lwdita-ast/src/nodes/video.ts
+++ packages/lwdita-ast/src/nodes/video.ts
@@ -19,23 +19,51 @@ import { FiltersNodeAttributes, FiltersFields, isValidFiltersField, makeFilters 
 import { ClassNodeAttributes, ClassFields, isValidClassField, makeClass } from "./class";
 import { ReuseNodeAttributes, ReuseFields, isValidReuseField, makeReuse } from "./reuse";
 import { LocalizationNodeAttributes, LocalizationFields, isValidLocalizationField, makeLocalization } from "./localization";
-import { areFieldsValid } from "../utils";
-import { makeComponent, AbstractBaseNode, BaseNode, makeAll, Constructor } from "./base";
+import { areFieldsValid, isOrUndefined } from "../utils";
+import { makeComponent, AbstractBaseNode, BaseNode, makeAll } from "./base";
 import { SizeFields, isValidSizeField, makeSize, SizeNodeAttributes } from "./size";
 import { BasicValue } from "../classes";
-import { CDATA, NMTOKEN } from "../ast-classes";
+import { BooleanString, CDATA, isBooleanString, isNMTOKEN, NMTOKEN, ReferenceContentScope } from "../ast-classes";
+import { isValidReferenceContentField, makeReferenceContent, ReferenceContentFields, ReferenceContentNodeAttributes } from "./reference-content";
 
 /**
  * Define all allowed `video` attributes:
- * `props`, `dir`, `xml:lang`, `translate`, `id`, `conref`, `outputclass`, `class`, `width`, `height`
+ * `props`, `dir`, `xml:lang`, `translate`, `id`, `conref`,
+ * `outputclass`, `class`, `width`, `height`, `href`,
+ * `format`, `scope`, `autoplay`, `controls`, `loop`, `muted`, `tabindex`
  */
-export const VideoFields = [...FiltersFields, ...LocalizationFields, ...ReuseFields, ...ClassFields, ...SizeFields];
+export const VideoFields = [
+  ...FiltersFields,
+  ...LocalizationFields,
+  ...ReferenceContentFields,
+  ...ReuseFields,
+  ...ClassFields,
+  ...SizeFields,
+  'autoplay',
+  'controls',
+  'loop',
+  'muted',
+  'tabindex'
+];
 
 /**
  * The interface `VideoNodeAttributes` defines all attribute types for `video`:
  * `CDATA`, `NMTOKEN`
  */
-export interface VideoNodeAttributes extends SizeNodeAttributes, FiltersNodeAttributes, LocalizationNodeAttributes, ReuseNodeAttributes, ClassNodeAttributes, BaseNode { }
+export interface VideoNodeAttributes extends
+  FiltersNodeAttributes,
+  LocalizationNodeAttributes,
+  ReferenceContentNodeAttributes,
+  ReuseNodeAttributes,
+  ClassNodeAttributes,
+  SizeNodeAttributes,
+  BaseNode {
+  'autoplay'?: BooleanString
+  'controls'?: BooleanString
+  'loop'?: BooleanString
+  'muted'?: BooleanString
+  'tabindex'?: NMTOKEN
+}
 
 /**
  * Check if the given attributes of the `video` node are valid
@@ -44,11 +72,25 @@ export interface VideoNodeAttributes extends SizeNodeAttributes, FiltersNodeAttr
  * @param value - A BasicValue-typed value containing the field value
  * @returns Boolean
  */
-export const isValidVideoField = (field: string, value: BasicValue): boolean => isValidFiltersField(field, value)
-  || isValidLocalizationField(field, value)
+export function isValidVideoField(field: string, value: BasicValue): boolean {
+  if (isValidLocalizationField(field, value)
+  || isValidFiltersField(field, value)
+  || isValidReferenceContentField(field, value)
   || isValidReuseField(field, value)
   || isValidClassField(field, value)
-  || isValidSizeField(field, value);
+  || isValidSizeField(field, value)
+  ) {
+    return true;
+  }
+  switch(field) {
+    case 'autoplay': return isOrUndefined(isBooleanString, value);
+    case 'controls': return isOrUndefined(isBooleanString, value);
+    case 'loop': return isOrUndefined(isBooleanString, value);
+    case 'muted': return isOrUndefined(isBooleanString, value);
+    case 'tabindex': return isOrUndefined(isNMTOKEN, value);
+    default: return false;
+  }
+}
 
 /**
  * Check if the `video` node is valid
@@ -68,8 +110,47 @@ export const isVideoNode = (value?: unknown): value is VideoNodeAttributes =>
  * @param constructor - The constructor
  * @returns A `video` node
  */
-export function makeVideo<T extends Constructor>(constructor: T): T {
-  return makeAll(constructor, makeLocalization, makeFilters, makeReuse, makeClass, makeSize);
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function makeVideo<T extends { new(...args: any[]): AbstractBaseNode }>(constructor: T): T  {
+  return makeAll(class extends constructor {
+    get 'autoplay'(): BooleanString {
+      return this.readProp<BooleanString>('autoplay');
+    }
+    set 'autoplay'(value: BooleanString) {
+      this.writeProp<BooleanString>('autoplay', value);
+    }
+    get 'controls'(): BooleanString {
+      return this.readProp<BooleanString>('controls');
+    }
+    set 'controls'(value: BooleanString) {
+      this.writeProp<BooleanString>('controls', value);
+    }
+    get 'loop'(): BooleanString {
+      return this.readProp<BooleanString>('loop');
+    }
+    set 'loop'(value: BooleanString) {
+      this.writeProp<BooleanString>('loop', value);
+    }
+    get 'muted'(): BooleanString {
+      return this.readProp<BooleanString>('muted');
+    }
+    set 'muted'(value: BooleanString) {
+      this.writeProp<BooleanString>('muted', value);
+    }
+    get 'tabindex'(): NMTOKEN {
+      return this.readProp<NMTOKEN>('tabindex');
+    }
+    set 'tabindex'(value: NMTOKEN) {
+      this.writeProp<NMTOKEN>('tabindex', value);
+    }
+  },
+    makeLocalization,
+    makeFilters,
+    makeReferenceContent,
+    makeReuse,
+    makeClass,
+    makeSize
+  );
 }
 
 /**
@@ -80,18 +161,10 @@ export function makeVideo<T extends Constructor>(constructor: T): T {
  * @param nodeName - A string containing the node name
  * @param isValidVideoField - A boolean value, if the field is valid or not
  * @param fields - A List of valid attributes @See {@link VideoFields}
- * @param childNodes - An Array of allowed child nodes: desc?', `video-poster?`, `media-controls?`, `media-autoplay?`, `media-loop?`, `media-muted?`, `media-source*`, `media-track*`
+ * @param childNodes - An Array of allowed child nodes: `desc?`, `fallback`, `video-poster?`, `media-source*`, `media-track*`
  */
-@makeComponent(makeVideo, 'video', isValidVideoField, VideoFields, ['desc?', 'video-poster?', 'media-controls?', 'media-autoplay?', 'media-loop?', 'media-muted?', 'media-source*', 'media-track*'])
+@makeComponent(makeVideo, 'video', isValidVideoField, VideoFields, ['desc?', 'fallback?', 'video-poster?', 'media-source*', 'media-track*'])
 export class VideoNode extends AbstractBaseNode implements VideoNodeAttributes {
-
-  // ClassNodeAttributes
-  'outputclass'?: CDATA
-  'class'?: CDATA
-
-  // ReuseNodeAttributes
-  'id'?: NMTOKEN
-  'conref'?: CDATA
 
   // LocalizationNodeAttributes
   'dir'?: CDATA
@@ -101,7 +174,27 @@ export class VideoNode extends AbstractBaseNode implements VideoNodeAttributes {
   // FiltersNodeAttributes
   'props'?: CDATA
 
+  // ReferenceContentNodeAttributes
+  'href'?: CDATA
+  'format'?: CDATA
+  'scope'?: ReferenceContentScope
+
+  // ReuseNodeAttributes
+  'id'?: NMTOKEN
+  'conref'?: CDATA
+
+  // ClassNodeAttributes
+  'outputclass'?: CDATA
+  'class'?: CDATA
+
   // SizeNodeAttributes
   'width'?: NMTOKEN
   'height'?: NMTOKEN
+
+  // VideoNodeAttributes
+  'autoplay'?: BooleanString
+  'controls'?: BooleanString
+  'loop'?: BooleanString
+  'muted'?: BooleanString
+  'tabindex'?: NMTOKEN
 }
