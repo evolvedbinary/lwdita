f8ccea77fd927401c191012acf26ab437dff8acb
marmoure <tinky93winky@gmail.com>
2025-03-20 19:48:20 +0100

    [test] update media track spec to match the dtd

--- packages/lwdita-ast/test/media-track.spec.ts
+++ packages/lwdita-ast/test/media-track.spec.ts
@@ -24,7 +24,7 @@ doNodeTest(
   'media-track',
   isMediaTrackNode,
   ['dir', 'xml:lang', 'translate', 'href', 'format', 'scope', 'keyref', 'kind', 'srclang', 'outputclass', 'class'],
-  ['text']
+  ['text*']
 );
 
 describe('Class MediaSourceNode', () => {
